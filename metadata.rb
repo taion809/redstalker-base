65db668124f819b695084b234ee4066e50dcfc21
Nicholas J <nicholas.a.johns5@gmail.com>
2013-11-10 13:10:00 +0000

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'redstalker-base'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures redstalker-base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
