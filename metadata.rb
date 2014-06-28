a741bcdeaf07940f40f12d6410cd1d1b26bd47ea
Nicholas Johns <taion809@users.noreply.github.com>
2014-06-28 12:18:59 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      'Installs/Configures redstalker-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.12'
 
-depends          'openssh'
 depends          'fail2ban'
