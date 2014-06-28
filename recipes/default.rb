9f32a793289951d297c580f5ea1d4c4096a37698
Nicholas Johns <taion809@users.noreply.github.com>
2014-06-28 12:15:45 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "redstalker-base::sysadmin_users"
+# include_recipe "redstalker-base::sysadmin_users"
 include_recipe "redstalker-base::cron"
-include_recipe "redstalker-base::metrics"
+# include_recipe "redstalker-base::metrics"
 include_recipe "redstalker-base::utilities"
